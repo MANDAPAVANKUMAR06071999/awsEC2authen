7ce20971fa3c3f5d6f3aafd751c1284eb5fbd6a6
MANDAPAVANKUMAR06071999 <pavankumarmanda6@gmail.com>
2022-04-01 09:00:42 +0530

    saving changes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const registerController = require('./Controllers/registerController');
+const loginController = require('./Controllers/loginController');
+const userController = require('./Controllers/userController');
+
+app.get('/home',(req,res)=>{
+    console.log("hello");
+    res.send("hello,there");
+})
+app.post('/register',registerController.registering);
+app.post('/login',loginController.logging_in);
+app.get('/user/:id',userController.retrievedata);
+app.post('/user/:id',userController.updatedata);
+
+
+// app.listen(7001,()=>{
+//     console.log("Server started and running at port number 7001");
+// })
+module.exports = app;
